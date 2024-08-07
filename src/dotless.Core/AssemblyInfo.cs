1aedac8a55f8484815deadd00554539904b0701c
pmason127 <patrick.g.mason@gmail.com>
2024-08-07 16:51:34 -0500

    oops, got to delete

--- src/dotless.Core/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-
-[assembly: AssemblyVersion("12.0.0.0")]
-[assembly: AssemblyFileVersion("12.0.0.0")]
